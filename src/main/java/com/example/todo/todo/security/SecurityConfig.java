f7affb3f64009512d6492a0bcce9d8bd7c6cb9c5
sijgn <saijoge@cy-com.co.jp>
2025-06-10 16:41:56 +0900

    SecurityConfigとLoginControllerにコメントを追加

--- src/main/java/com/example/todo/todo/security/SecurityConfig.java
+++ src/main/java/com/example/todo/todo/security/SecurityConfig.java
@@ -11,24 +11,32 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-       http
+       http // ログインしていない状態でどのページにもアクセスできなくなる
             .authorizeHttpRequests(request -> request
+            // リクエストの特定の条件に一致するかどうかを判定
                 .requestMatchers("/css/**", "/images/**", "/js/**", "/register/**").permitAll()
                 .anyRequest().authenticated())
             .formLogin(login -> login
-                .loginProcessingUrl("/login")
-                .loginPage("/login")
-                .defaultSuccessUrl("/nagomi")
-                .failureUrl("/login?error")
-                .permitAll()
+                // ログイン認証の送信先URL（POST送信）
+                .loginProcessingUrl("/login") 
+                // ログインページのURL（GET送信）
+                .loginPage("/login") 
+                // 認証成功時に遷移するページ
+                .defaultSuccessUrl("/nagomi") 
+                // 認証失敗時に遷移するページ
+                .failureUrl("/login?error") 
+                // ログインページを認証不要でアクセス可能にする設定
+                .permitAll() 
             )
             .logout(logout -> logout
+                // ログアウト後に遷移するページ
                 .logoutSuccessUrl("/login")
                 .permitAll()
             );
         return http.build();
     }
 
+    // ＠BeanによってDIコンテナに登録され、他のクラスでDIを利用できる
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
