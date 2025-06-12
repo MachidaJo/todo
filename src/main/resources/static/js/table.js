77c52029e2059f5de1422a6ffa8ba179891ed310
Omori <ohmorire@cy-com.co.jp>
2025-06-12 12:10:03 +0900

    テーブルの幅変更

--- src/main/resources/static/js/table.js
+++ src/main/resources/static/js/table.js
@@ -1,19 +1,20 @@
-// ページがロードされたときに実行
 window.onload = function() {
     // 全てのタイトル列を取得
-    let titleCells = document.querySelectorAll('.title-width');
+    let todoTitles = document.querySelectorAll('.todo-table .auto-width');
+    let doneTitles = document.querySelectorAll('.done-table .auto-width');
+    let allTitles = [...todoTitles, ...doneTitles];
 
     // 各タイトルの幅を確認し、最大の幅を取得
-    let maxWidth = 0;
-    titleCells.forEach(cell => {
-        let cellWidth = cell.offsetWidth;
+    let maxWidth = 110;
+    allTitles.forEach(cell => {
+        let cellWidth = cell.scrollWidth; // 使用する幅をscrollWidthで取得
         if (cellWidth > maxWidth) {
             maxWidth = cellWidth;
         }
     });
 
     // 最大の幅を全てのタイトル列に適用
-    titleCells.forEach(cell => {
+    allTitles.forEach(cell => {
         cell.style.width = maxWidth + 'px';
     });
 };
