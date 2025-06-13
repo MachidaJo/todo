af5279dcb0c98143b4da0fd9cdee843139fb57fe
machidajo <jomachida@cy-com.co.jp>
2025-06-13 14:33:33 +0900

    アカウント作成時にサンプルデータを塚するように変更。また、成功メッセージも追加しました。

--- src/main/java/com/example/todo/todo/Service/TodoService.java
+++ src/main/java/com/example/todo/todo/Service/TodoService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 
 import com.example.todo.todo.entity.Todo;
+import com.example.todo.todo.entity.User;
 import com.example.todo.todo.repository.TodoRepository;
 import com.example.todo.todo.security.CustomUserDetails;
 
@@ -30,6 +31,12 @@ public class TodoService {
     public void createTodo(@AuthenticationPrincipal CustomUserDetails userDetails, Todo todo) {
         // ユーザーIDと未完了フラグにして作成
         todo.setUserId(userDetails.getUserId());
+        todo.setCompleted(false);
+        todoRepository.insertTodo(todo);
+    }
+
+    public void createTodo(User user, Todo todo) {
+        todo.setUserId(user.getUserId());
         todo.setCompleted(false);
         todoRepository.insertTodo(todo);
     }
--- src/main/java/com/example/todo/todo/controller/LoginController.java
+++ src/main/java/com/example/todo/todo/controller/LoginController.java
@@ -1,13 +1,16 @@
 package com.example.todo.todo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class LoginController {
     // ログイン画面をルーティング
     @GetMapping("/login")
-    public String login() {
+    public String login(@ModelAttribute("message") String message, Model model) {
+        model.addAttribute("message", message);
         return "login";
     }
 }
